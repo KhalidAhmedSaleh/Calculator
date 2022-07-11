860b9ed577e4823ba40c5e92016dc36c286bc817
Khalid <khalid.thereader7@gmail.com>
2022-07-11 16:50:29 +0300

    Fixing pdf logic

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 'use strict';
 
-document.getElementById('result').readOnly = true; //set this attribute in Html file
+const numbers = document.querySelectorAll('.number'); // Selecting all elements have number class
+let result = document.getElementById('result'); // Select the input filed by its ID
+const operations = document.querySelectorAll('.operation'); // Selecting all elements have operation class
 
-const numbers = document.querySelectorAll('.number');
-let result = document.getElementById('result');
-
-const operations = document.querySelectorAll('.operation');
+const operationsArr = [];
+let pdfString = '';
 
 let num = 0;
-
+const pdfButton = document.querySelector('.PDF');
 let screenValue = '';
-let equalResult = 0;
+
+// Looping over the numbers to get the element
 for (let number = 0; number < numbers.length; number++) {
   numbers[number].addEventListener('click', function (e) {
     num = e.target.innerText;
-    console.log(num);
     result.value += num;
-    console.log(number);
   });
 }
 
 for (let i = 0; i < operations.length; i++) {
   operations[i].addEventListener('click', function () {
-    console.log(i);
+    //* 'i' variable will give me the index of an operation and based on that i will add the operation
     if (i === 1) {
-      screenValue = '*';
-      result.value += screenValue;
+      result.value += '*';
     } else if (i === 0) {
       result.value = '';
-    } else if (i === 2) {
-      screenValue = '/';
-      result.value += screenValue;
     } else if (i === 6) {
-      screenValue = '(';
-      result.value += screenValue;
+      result.value += '(';
     } else if (i === 2) {
-      screenValue = '/';
-      result.value += screenValue;
+      result.value += '/';
     } else if (i === 7) {
-      screenValue = ')';
-      result.value += screenValue;
+      result.value += ')';
     } else if (i === 8) {
-      screenValue = '%';
-      result.value += screenValue;
+      result.value += '%';
     } else if (i === 4) {
-      screenValue = '+';
-      result.value += screenValue;
+      result.value += '+';
     } else if (i === 5) {
-      screenValue = '-';
-      result.value += screenValue;
+      result.value += '-';
     } else if (i === 3) {
-      screenValue = '.';
-      result.value += screenValue;
+      result.value += '.';
     } else if (i === 9) {
+      // * Once click on equal sign the porgram will add everthing on the input to an array then eval all numbers
+      operationsArr.push(result.value);
       screenValue = eval(result.value);
       result.value = screenValue;
     }
   });
 }
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Backspace') {
-    screenValue = screenValue.slice(0, -1);
-    result.value = screenValue;
-  }
+pdfButton.addEventListener('click', function () {
+  let doc = new jsPDF();
+  arrayLines(operationsArr, doc);
+  doc.save('operations.pdf');
 });
+
+function arrayLines(arrayToLine, doc) {
+  let idx = 0;
+  let x = 10;
+  let y = 10;
+  while (idx < arrayToLine.length) {
+    doc.text(arrayToLine[idx], x, y);
+    y += 10;
+    idx++;
+  }
+}
